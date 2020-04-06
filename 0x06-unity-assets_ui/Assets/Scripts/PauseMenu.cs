26703fdf5982a8073f8df0fe33bdaeb11ca3d2dc
Martin Smith <mcodesmith@gmail.com>
2020-04-05 22:09:44 -0700

    Added Pause menu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Timer timer;
+    
+
+    private bool _isPaused;
+
+    private void Awake()
+    {
+        PauseSingleton.Instancce.Menu = this.gameObject;
+    }
+
+    public bool IsPaused
+    {
+        get => _isPaused;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        _isPaused = timer.IsPaused = true;
+        gameObject.SetActive(true);
+    }
+
+   
+
+    public void Resume()
+    {
+        _isPaused = timer.IsPaused = false;
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        PauseSingleton.Instancce.Dispose();
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        gameObject.SetActive(false);
+        if (SceneManager.sceneCount < 2)
+        {
+            SceneManager.LoadScene("Scenes/Options", LoadSceneMode.Additive);
+        }
+    }
+}
+
+public sealed class PauseSingleton : IDisposable
+{
+    private static PauseSingleton _instancce = null;
+    public static readonly object padlock = new object();
+
+    private GameObject _menu;
+
+    PauseSingleton()
+    {
+    }
+
+    public static PauseSingleton Instancce
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (_instancce == null)
+                    _instancce = new PauseSingleton();
+                return _instancce;
+            }
+        }
+    }
+
+
+    public GameObject Menu
+    {
+        get => _menu;
+        set => _menu = value;
+    }
+
+
+    public void Dispose()
+    {
+        _instancce = null;
+    }
+}
