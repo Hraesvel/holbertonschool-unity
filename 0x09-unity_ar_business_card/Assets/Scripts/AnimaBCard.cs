2fa2bcecbe95a2bc5b28ab58d3522ab7997f0376
Martin Smith <mcodesmith@gmail.com>
2020-05-20 12:07:31 -0700

    pushing project to repo

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/AnimaBCard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class AnimaBCard : MonoBehaviour, ITrackableEventHandler
+{
+
+    private TrackableBehaviour _mTrackableBehaviour;
+    
+    private static readonly int OpenMenu = Animator.StringToHash("OpenMenu");
+    [SerializeField] private Animator anim;
+    
+    
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        Debug.Log("Track state changed to something!");
+
+        if (!anim)
+            return;
+
+        if (
+            newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED
+        )
+        {
+            
+            if (anim.GetBool(OpenMenu)) return;
+            Debug.Log("Opening Menu");
+            anim.SetBool(OpenMenu, true);
+        }
+        else
+        {
+            anim.Play("Start", -1, 0f);
+            anim.SetBool(OpenMenu, false);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (_mTrackableBehaviour)
+        {
+            _mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+    
+}
---
+++ 0x09-unity_ar_business_card/README.md
@@ -0,0 +1,3 @@
+# 0x09. Unity - AR Business Card
+
+simple vuforia AR
