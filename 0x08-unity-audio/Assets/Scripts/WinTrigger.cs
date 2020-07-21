9e5640725b8564d48a5c0e0d842d5c06161ea10c
Martin Smith <mcodesmith@gmail.com>
2020-07-21 12:38:57 -0700

    win trigger will stop BGM and play win jingle in its place

--- 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
+++ 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class WinTrigger : MonoBehaviour
 {
     [SerializeField] private Timer _timer;
+    [SerializeField] private AudioSource BGM;
+    [SerializeField] private AudioClip WinClip;
 
 
     private void Start()
     {
         if (_timer == null)
             _timer = FindObjectOfType<Timer>();
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,6 +25,10 @@ public class WinTrigger : MonoBehaviour
         {
             _timer.Win();
             Time.timeScale = 0;
+           BGM.Stop();
+           BGM.time = 0;
+           BGM.clip = WinClip;
+           BGM.Play();
         }
         
     }
